f886f5fda5908ef9ec045a393cf50a6291d37b92
Siqian <liu00638@algonquinlive.com>
2024-06-08 11:47:11 -0400

    add SnackBar, AlertDialog to save or clear login data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Lab3'),
     );
   }
 }
@@ -31,18 +33,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  var isChecked = false;
   late TextEditingController _loginController;
   late TextEditingController _passwordController;
   bool _isPasswordVisible = false;
   String _password = '';
   String? _passwordLabel;
   String _imageSource = 'images/question-mark.png';
+  late EncryptedSharedPreferences savedData;
 
   @override
   void initState() {
     super.initState();
     _loginController = TextEditingController();
     _passwordController = TextEditingController();
+
+    savedData = EncryptedSharedPreferences(); //constructor is not asynchronous
+    savedData.getString("loginName").then( (unencryptedString)  {
+      if(unencryptedString != null){
+        _loginController.text = unencryptedString;
+      }
+    });
+
+    savedData.getString("loginPassword").then( (unencryptedString)  {
+      if(unencryptedString != null){
+        _passwordController.text = unencryptedString;
+
+        //show Snackbar here
+
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text('the previous login name and passwords have been loaded'),
+                action: SnackBarAction( label:'Clear saved data',
+                    onPressed: () {
+                  savedData.remove("loginName");
+
+                      savedData.remove("loginPassword");
+                      _loginController.text= '';
+                      _passwordController.text='';
+
+
+                } ),
+                duration: Duration(seconds: 30)));
+
+      }
+    });
+
   }
 
   @override
@@ -52,24 +89,24 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  void _login() {
-    _password = _passwordController.text;
-
-    setState(() {
-      _isPasswordVisible = true;
-      _passwordLabel = 'Password';
-
-      if (_password == 'QWERTY123') {
-        _imageSource = 'images/idea.png';
-      }
-      else {
-        _imageSource = 'images/stop.png';
-      }
-    });
+  void functionName() async {
+    final prefs = await SharedPreferences.getInstance();
   }
+
+  void _clearData () {
+    savedData.remove("loginName");
+    savedData.remove("loginPassword");
+    _loginController.text= '';
+    _passwordController.text='';
+    Navigator.pop(context);
+
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
+    //var widget;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -79,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(controller: _loginController,
+            TextField(controller:  _loginController,
                 decoration: const InputDecoration(
                     hintText:"Login",
                     border: OutlineInputBorder()
@@ -91,13 +128,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   labelText: _passwordLabel
                 ),
                 obscureText: !_isPasswordVisible,
-    /*onChanged: (value) {
-                if (_isPasswordVisible) {
-                    setState(() {
-                      _password = value;
-                    });
-                  }
-                }*/),
+
+            ),
 
 
             ElevatedButton( onPressed: _login,
@@ -112,4 +144,49 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+void _login() {
+  _password = _passwordController.text;
+
+  setState(() {
+    _isPasswordVisible = true;
+    _passwordLabel = 'Password';
+
+    if (_password == 'QWERTY123') {
+      _imageSource = 'images/idea.png';
+    }
+    else {
+      _imageSource = 'images/stop.png';
+    }
+  });
+
+  showDialog<String>(
+    context: context,
+    builder: (BuildContext ctx) =>
+        AlertDialog(
+          title: const Text('Save data'),
+          content: const Text('Do you want to save your login'),
+          actions: <Widget>[
+            ElevatedButton(child: Text("Yes"),
+              onPressed: () {
+                var nameTyped = _loginController.value.text; //
+                var passwordTyped = _passwordController.value.text; //
+
+                //savedData is EncryptedSharedPreferences
+                savedData.setString("loginName", nameTyped); //variable name
+                savedData.setString("loginPassword", passwordTyped); //variable name
+                Navigator.pop(context);
+              }
+              ,),
+            FilledButton(child: Text("No"),
+                onPressed: _clearData),
+
+          ],
+        ),
+  );
+
+
 }
+
+}
+
